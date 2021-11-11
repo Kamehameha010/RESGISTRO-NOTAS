bc407704a3c620f90435606525157341049ae422
Kamehameha010 <59071232+Kamehameha010@users.noreply.github.com>
2021-11-11 00:46:09 -0600

    Delete C#/WsSchool.Core directory

--- C#/WsSchool.Core/Models/DTOs/CoursesStatusDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WsSchool.Core.Models.DTOs
-{
-    public class CoursesStatusDTO
-    {
-        public int CourseStatusId { get; set; }
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-}
--- C#/WsSchool.Core/Models/DTOs/TeacherDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace WsSchool.Core.Models.DTOs
-{
-    public class TeacherDTO
-    {
-        public int TeacherId { get; set; }
-        public int? PersonId { get; set; }
-        public string Subject { get; set; }
-    }
-}
